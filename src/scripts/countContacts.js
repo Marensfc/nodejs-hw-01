0f891db48b56ef0cd30b6d00c1c9e04540c5cee2
Marensfc <alexeymaren12@gmail.com>
2024-06-04 21:48:05 +0300

    Made 5 scripts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const result = await fs.readFile(PATH_DB, 'utf8');
+    const contactsCount = JSON.parse(result).length;
+    return contactsCount;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  const data = [];
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await removeAllContacts();
