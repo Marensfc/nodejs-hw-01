7aed625c6a54a7e18d71d0d5bcf2265940d00531
Marensfc <alexeymaren12@gmail.com>
2024-06-04 22:20:43 +0300

    Made homework

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf8');
+
+    const newContacts = JSON.parse(contacts).filter((contact) => {
+      const randomBoolean = Boolean(Math.round(Math.random() * 2));
+      return randomBoolean;
+    });
+
+    fs.writeFile(PATH_DB, JSON.stringify(newContacts));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await thanos();
