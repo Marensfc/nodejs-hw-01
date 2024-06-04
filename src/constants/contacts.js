520194c07807c4e3bcba46a6677c996999ee7915
Marensfc <alexeymaren12@gmail.com>
2024-06-04 14:48:49 +0300

    Generate contacts

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from 'node:path';
+
+const workDirectory = process.cwd();
+
+export const PATH_DB = path.join(workDirectory, 'src', 'db', 'db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let contacts = [];
 
-await generateContacts(5);
+  for (let i = 0; i < number; i += 1) {
+    contacts.push(createFakeContact());
+  }
+
+  try {
+    const result = await fs.readFile(PATH_DB, 'utf8');
+    const data = [...JSON.parse(result), ...contacts];
+    await fs.writeFile(PATH_DB, JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+await generateContacts(2);
